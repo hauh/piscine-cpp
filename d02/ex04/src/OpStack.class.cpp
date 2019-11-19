8bf31880ff40fcff3a20a72c7b81910dfff8861f
smorty <hauh@ya.ru>
2019-11-20 00:11:07 +0300

    makefile

--- d02/ex04/src/OpStack.class.cpp
+++ d02/ex04/src/OpStack.class.cpp
@@ -6,7 +6,7 @@
 /*   By: smorty <smorty@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/19 18:41:00 by smorty            #+#    #+#             */
-/*   Updated: 2019/11/19 18:56:01 by smorty           ###   ########.fr       */
+/*   Updated: 2019/11/20 00:09:29 by smorty           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,15 +18,11 @@ void	OpStack::push(char op)
 {
 	if (_top)
 	{
-		s_opstack *insert = new s_opstack {op};
-		insert->next = _top;
+		s_opstack *insert = new s_opstack {op, _top};
 		_top = insert;
 	}
 	else
-	{
-		_top = new s_opstack {op};
-		_top->next = nullptr;
-	}
+		_top = new s_opstack {op, nullptr};
 }
 
 char	OpStack::pop(void)
